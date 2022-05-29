a636f52ab2d1c701d1f5baeff7bc823bdf4dbf6b
Joshua Allard <joshuaal2000@gmail.com>
2022-05-29 12:41:09 -0400

    Adds remaining duration display to comps page

--- src/app/competitions/competitions.component.ts
+++ src/app/competitions/competitions.component.ts
@@ -22,8 +22,12 @@ export class CompetitionsComponent {
     return sign + `${formatted}`;
   };
 
+  getSecondsRemaining = (time: Date) => {
+    return (time.getTime() - new Date().getTime()) / 1000;
+  }
+
   getTimeRemaining = (time: Date) => {
-    const timeSeconds = (time.getTime() - new Date().getTime()) / 1000;
+    const timeSeconds = this.getSecondsRemaining(time);
     if(timeSeconds < 0) {
       return '';
     }
@@ -38,16 +42,38 @@ export class CompetitionsComponent {
   };
 
   durationFormatter = (timeSeconds: number) => {
-    return `${timeSeconds / 60}m`;
+    if(timeSeconds < 0) {
+      return '';
+    }
+    const minutes = Math.floor(timeSeconds / 60);
+    const seconds = Math.floor(timeSeconds % 60);
+    if(seconds) {
+      return `${minutes}m ${seconds}s`;
+    }
+    return `${minutes}m`;
   };
 
   private gridAPI: GridApi;
   columnDefs: ColDef[] = [
     { field: 'city' } as ColDef,
     { field: 'resource' },
-    { field: 'startTime', filter: 'agNumberColumnFilter', valueFormatter: params => this.timeFormatter(params.data.startTime) },
-    { field: 'startsIn', filter: 'agNumberColumnFilter', valueFormatter: params => this.getTimeRemaining(params.data.startsIn) },
-    { field: 'duration', filter: 'agNumberColumnFilter', valueFormatter: params => this.durationFormatter(params.data.duration) },
+    { field: 'startTime', filter: 'agNumberColumnFilter', valueFormatter: params => this.timeFormatter(params.value) },
+    { field: 'startsIn', filter: 'agNumberColumnFilter', valueFormatter: params => this.getTimeRemaining(params.value) },
+    { field: 'durationLeft', filter: 'agNumberColumnFilter', valueFormatter: params => this.durationFormatter(params.value), headerName: 'Duration Left',
+      cellRenderer: params => {
+        let duration = params.value;
+        if (!duration) {
+          return '';
+        }
+        const secondsFromStartTime = this.getSecondsRemaining(params.data.startTime);
+        let color = '#CCCCCC59';
+        if(secondsFromStartTime < 0) {
+          color = '#00aa0059';
+        }
+        const percent = Math.floor(100 * duration / 3600);
+        return `<p style="background: linear-gradient(90deg, ${color} ${percent}%, #00000000 0%);">${this.durationFormatter(duration)}</p>`;
+      }
+    },
     { field: 'rewardType' },
     { field: 'rewardMoney', filter: 'agNumberColumnFilter', valueFormatter: params => this.currencyFormatter(params.data.rewardMoney, '$'), headerName: 'Money' },
     { field: 'rewardPrestige', filter: 'agNumberColumnFilter', headerName: 'Prestige' },
@@ -89,6 +115,17 @@ export class CompetitionsComponent {
     event.api.sizeColumnsToFit();
     this.loadData().then();
     setInterval(() => {
+      this.rowData.forEach(comp => {
+        let duration = comp.duration;
+        if (!duration) {
+          return '';
+        }
+        const secondsFromStartTime = this.getSecondsRemaining(comp.startTime);
+        if(secondsFromStartTime < 0) {
+          duration += secondsFromStartTime;
+        }
+        comp.durationLeft = duration;
+      })
       event.api.refreshCells();
     }, 500)
     
